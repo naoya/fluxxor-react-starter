85f1fdaac9b9196074d4780c8a36b9f9458a4fb2
Naoya Ito <i.naoya@gmail.com>
2015-03-20 15:29:35 +0900

    Enable sourcd map

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,17 @@ var connect    = require('gulp-connect');
 var browserify = require('browserify');
 var reactify   = require('reactify');
 var source     = require('vinyl-source-stream');
+var buffer     = require('vinyl-buffer');
 var watchify   = require('watchify');
+var sourcemaps = require('gulp-sourcemaps');
 
 function errorHandler (err) {
   util.log(util.colors.red('Error'), err.message);
   this.end();
 }
+
+var bundler = watchify(browserify({ entries: './jsx/app.jsx', debug: true } , watchify.args));
+bundler.transform(reactify);
 
 gulp.task('connect', function() {
   connect.server({
@@ -19,22 +24,23 @@ gulp.task('connect', function() {
 });
 
 gulp.task('reload', function () {
-  gulp.src('./public/**/*.*')
+  gulp.src(['./public/**/*.*', '!./public/**/*.js.map'])
       .pipe(connect.reload());
 });
 
-gulp.task('build', function() {
-  var bundler = watchify(browserify('./jsx/app.jsx', watchify.args));
-  bundler.transform(reactify);
+gulp.task('build', function () {
   bundler.bundle()
       .on('error', errorHandler)
       .pipe(source('app.js'))
-      .pipe(gulp.dest('./public/js'));
+        .pipe(buffer())
+        .pipe(sourcemaps.init({ loadMaps: true  }))
+        .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./public/js'));    
 });
 
 gulp.task('watch', function () {
   gulp.watch(['./jsx/**/*.jsx'], ['build']);
-  gulp.watch(['./public/**/*.*'], ['reload']);
+  gulp.watch(['./public/**/*.*', '!./public/**/*.js.map'], ['reload']);
 });
 
 gulp.task('default', ['build', 'connect', 'watch']);
--- jsx/app.jsx
+++ jsx/app.jsx
@@ -1,4 +1,4 @@
-var React = require('react');
+var React   = require('react');
 var Fluxxor = require('fluxxor');
 
 var actions = {};
